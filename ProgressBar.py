a2e2ef54c136331cbf6893704103924b0d57c59c
RobertKajnak <hezirisz@gmail.com>
2018-06-26 14:17:08 +0200

    Complete rework of percent and time estimation
    The percentage is now calculated, and printed to the second decimal. The only factor that matters for percentage calculation ison display is the display interval. Now also works with <100 iterations and works more precisely on other values

--- ProgressBar.py
+++ ProgressBar.py
@@ -34,8 +34,6 @@ class ProgressBar:
             'ascii' - to be sure it works on older consoles
             'kao'   - full faces using UTF-8 characters
             'off'   - less emotional, more professional
-    skipPercent -- the pecentage that are displayed. Takes priority over time 
-                   estimate
             1   -> display every percentage
             10  -> display 10%,20%...
             '''
@@ -52,22 +50,16 @@ class ProgressBar:
     
     
     def __init__(self,totalIterationCount, displayInterval=2,
-                      sound='auto', emoji='kao', skipPercent=1):
+                      sound='auto', emoji='kao'):
 
         if totalIterationCount<1:
             raise ValueError('The number of iterations can not be <=0')
         self.n = totalIterationCount  
-    
         self.currentIter = 0
-        self.currentPercent = 0
-        
-        self.percent = self.n/100
-        self.skipPercent = skipPercent if skipPercent>0 else 1
-        
+                    
         self.displayInterval = displayInterval
                 
         self.sound = sound
-        
         self.emoji = emoji
 
     def checkProgress(self):
@@ -80,61 +72,54 @@ class ProgressBar:
         
         #Set starting time        
         if self.currentIter==0:
-            print('Initializing Variables, calculating time estimate',end='\n')   
+            print('Initializing Variables, calculating time estimate',end='')   
             self.t_start = t_now
             #The estimation will be guaranteed on first percent
             self.lastDisplayed = -self.displayInterval
+            #this way the next if only has to check one condition, not two.
+            #Marginal or irrelevant gains, I know
+            self.currentIter = 1
+            return
             
         #increment iterator    
-        self.currentIter += 1 
-        
-        #TODO - create subpercentages     
-		#TODO - add check for processes that have 1%<10s print time
-        
-        #if a percentage mark is hit
-        if self.currentIter % self.percent == 0:
-            self.currentPercent += 1;
-            
-            #chek if it needs to be skipped
-            if self.currentPercent % self.skipPercent == 0:
+        self.currentIter += 1
                 
-                #check if enough time has passed since last precentage  print
-                if t_now - self.lastDisplayed > self.displayInterval:
-                    #print precentage
-                    self._erase_prev_output()
-                    print('\r%d%% complete. '%(self.currentPercent),end='')
-                    #update time
-                    self.lastDisplayed = t_now
+        #check if enough time has passed since last precentage print
+        if t_now - self.lastDisplayed > self.displayInterval:
+            #print precentage
+            currentPercent = 100.0*self.currentIter / self.n
+            self._erase_prev_output()
+            print('\r%.2f%% complete. '%(currentPercent),end='')
+            #update time
+            self.lastDisplayed = t_now
 
-                    #Calculate and display remaining time
-                    ahead = (100-self.currentPercent)*(t_now - self.t_start) / \
-                                self.currentPercent
-                    time_remaining_string = self._generate_time_string(ahead);
-                    print('Estimated time remaining: %s ' % time_remaining_string,end='')
-                    self._print_emoji()
-                            
-
-                if self.sound=='auto':
-                    self._set_best_sound(ahead);
-                
+            #Calculate and display remaining time
+            ahead = (self.n-self.currentIter)*(t_now - self.t_start) / \
+                        self.currentIter
+            time_remaining_string = self._generate_time_string(ahead);
+            print('Estimated time remaining: %s ' % time_remaining_string,end='')
+            self._print_emoji(currentPercent)                
                 
         if self.currentIter == self.n-1:
-            time_it_took = self._generate_time_string(t_now-self.t_start)
+            time_it_took = self._generate_time_string(t_now-self.t_start,'%.2f')
             self._erase_prev_output()
-            print('\r100%% -- Finished!',end='')
-            self._print_emoji(True)
+            print('\r100% -- Finished!',end='')
+            self._print_emoji(100)
             print(' The process took %s '% time_it_took)
+            
+            if self.sound=='auto':
+                self._set_best_sound(t_now-self.t_start);
             self.playTune()
             
     def _erase_prev_output(self):
         for i in range(1,50):
             print('\b \b',end='')
             
-    def _print_emoji(self,isComplete=False):
+    def _print_emoji(self,currentPercent):
         '''Prints the appropriate emoji for the percentage'''
         if self.emoji in self.emojis:
-            if not isComplete:
-                print(self.emojis[self.emoji][int(self.currentPercent/25)],end='')
+            if int(currentPercent)!=100:
+                print(self.emojis[self.emoji][int(currentPercent/25)],end='')
             else:
                 print(self.emojis[self.emoji][-1],end='')
 
@@ -208,42 +193,53 @@ class ProgressBar:
                         (13, 1),('b',1),('a',1),('g',1),('f',2),('a',2),
                         ('g',1),('f',1),('e',1),('d',1),('c',2),('c',2)]
                 ,tempo=200)
-            
+        if self.sound=='tune2':
+            self._play([('c#',2),('f#',1),('a',2),('f#',1),('f',2),
+                        ('f#',1),('g#',1/4.0),('f#',2),
+                        ('d',2),('e',1/4.0),('f#',1/4.0),('c#',2),
+                        ('b3',0.25),('a3',0.25),('a3',0.25),('g#3',0.25),('g#3',0.75),
+                        ('c#',0.5),('f#3',2)                        
+                        ],tempo=240)
         return
 
 
     '''Private:'''
-    def _generate_time_string(self,time):
+    def _generate_time_string(self,time,seconds_format='%d'):
         '''format it into a h:m:s format'''
-        #seconds is always a fractional value, therefore it is 
-        #always plural
-        time_string = '%.2f seconds'%(time%60)
+        
+        time_string = (seconds_format+' second')%(time%60)
+        if int(time)!=1:
+            time_string = time_string + 's';
+            
         if time%3600 >= 60:
             if time%3600 < 120:
                 time_string ='%d minunte '%((time/60)%60) \
                                     + time_string
             else:
                 time_string ='%d minuntes '%((time/60)%60) \
-                                    + time_string
-                                    
+                                    + time_string              
         if time>=3600:
             if time<7200:
                 time_string = '%d hour '%(time/3600)  \
                                     + time_string
             else:
-                time_string = '%d hour '%(time/3600)  \
+                time_string = '%d hours '%(time/3600)  \
                                     + time_string
         return time_string
 
 if __name__ == "__main__":
-    n = 12000;
+    
+    #number of iterations    
+    n = 33;
+    
+    #tune the length of the fake work -- stand-in for 'sleep'
+    m = 10000000;
     #using default parameters
     pb = ProgressBar(n);
     for i in range(1,n):
         pb.checkProgress();
         
         k=0;
-        for j in range(1,n):
+        for j in range(1,m):
             k=k+1;
-        
         
